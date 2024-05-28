96fc59cc096e775989e10888d407cc3a2c632e13
Chris Mountford <chris@think.net.au>
2024-05-28 10:20:02 +1000

    tweak

--- colour-prototype/src/la.ts
+++ colour-prototype/src/la.ts
@@ -3,13 +3,16 @@
 Linear algebra functions as required.
  */
 
+/** Function on all elements of an array implementation of a tensor of T */
+type all<T> = (ts:T[]) => T[];
+
 /**
  * https://en.wikipedia.org/wiki/Softmax_function
- * @param vec
+ * @param a
  */
-function softmax(vec: number[]) {
-    let denom = vec.reduce((a, b) => a + Math.exp(b), 0);
-    return vec.map(x => Math.exp(x) / denom);
+const softmax: all<number> = (a: number[]) => {
+    let denom = a.reduce((a, b) => a + Math.exp(b), 0);
+    return a.map(x => Math.exp(x) / denom);
 }
 
 const relu: all<number> = (a: number[]) => a.map(x => x > 0 ? x : 0)
