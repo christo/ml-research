6c0e9cf9ee7a0b3f8229d0800f3c4662d1c87a58
Chris Mountford <chris@think.net.au>
2024-05-28 10:18:18 +1000

    add some colour data

--- colour-prototype/src/colours.ts
+++ colour-prototype/src/colours.ts
@@ -2,12 +2,12 @@
 Adapted from https://css-tricks.com/converting-color-spaces-in-javascript/
  */
 
-type Hsl = { h: number, s: number, l: number };
+export type Hsl = { h: number, s: number, l: number };
 
 /**
  * Convert rgb components in range 0-1 to hsl in range 0-360, 0-100, 0-100
  */
-function rgbToHsl(r: number, g: number, b: number): Hsl {
+export function rgbToHsl(r: number, g: number, b: number): Hsl {
     // Find greatest and smallest channel values
     let cmin = Math.min(r, g, b);
     let cmax = Math.max(r, g, b);
@@ -42,13 +42,13 @@ function rgbToHsl(r: number, g: number, b: number): Hsl {
 /**
  * Format hsl as css expression
  */
-function hslToCss(hsl: { s: number; h: number; l: number }) {
+export function hslToCss(hsl: { s: number; h: number; l: number }) {
     return "hsl(" + hsl.h + "," + hsl.s + "%," + hsl.l + "%)";
 }
 
 /**
  * Convert components in range 0-255 to hsl object.
  */
-function Rgb255Hsl(red: number, green: number, blue: number): Hsl {
+export function rgb255ToHsl(red: number, green: number, blue: number): Hsl {
     return rgbToHsl(red / 255, green / 255, blue / 255);
 }
