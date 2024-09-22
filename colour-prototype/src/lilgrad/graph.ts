c2912636a1c14db58ec5375baf42f309155e7766
Chris Mountford <chris.mountford@gmail.com>
2024-09-22 13:27:02 +1000

    added notes on subjective metacognitition

---
+++ colour-prototype/src/lilgrad/graph.ts
@@ -0,0 +1,15 @@
+import {Value} from "./engine";
+
+type Node = {
+    labels: string[], // probably only two
+    edges: Node[]
+}
+
+
+function trace(root: Value) {
+
+}
+
+function drawDiGraph(root: Node) {
+
+}
